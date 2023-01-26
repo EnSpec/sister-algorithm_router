49cb1835b084a252f3214aae5ab4844e41e305ad
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-01-26 15:26:03 -0800

    Skip snowgrain

--- router.py
+++ router.py
@@ -39,21 +39,20 @@ def main():
             queue="sister-job_worker-16gb",
             identifier="")
 
+    # # Snow grain size PGE
+    # if (snow_mask.sum() >= run_config['inputs']['min_pixels']) & ("DESIS" in fc_base_name):
 
-    # Snow grain size PGE
-    if (snow_mask.sum() >= run_config['inputs']['min_pixels']) & ("DESIS" in fc_base_name):
-
-        grainsize = maap.submitJob(
-            algo_id="sister-grainsize",
-            version="sister-dev",
-            l2a_rfl= run_config['inputs']['l2a_rfl'],
-            l2a_frcover= run_config['inputs']['l2a_frcover'],
-            snow_cover = run_config['inputs']['snow_cover'],
-            CRID= run_config['inputs']['CRID'],
-            publish_to_cmr=False,
-            cmr_metadata={},
-            queue="sister-job_worker-16gb",
-            identifier="")
+    #     grainsize = maap.submitJob(
+    #         algo_id="sister-grainsize",
+    #         version="sister-dev",
+    #         l2a_rfl= run_config['inputs']['l2a_rfl'],
+    #         l2a_frcover= run_config['inputs']['l2a_frcover'],
+    #         snow_cover = run_config['inputs']['snow_cover'],
+    #         CRID= run_config['inputs']['CRID'],
+    #         publish_to_cmr=False,
+    #         cmr_metadata={},
+    #         queue="sister-job_worker-16gb",
+    #         identifier="")
 
 
 
