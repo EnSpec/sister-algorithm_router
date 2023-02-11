eb30dfd1995100ca96a0055026262297fa5648db
Sujen Shah <sujen1412@gmail.com>
2023-02-11 01:13:06 -0800

    Fix path to maap-py

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+basedir=$( cd "$(dirname "$0")" ; pwd -P )
+pushd $basedir
 git clone --single-branch --branch sister-dev https://gitlab.com/geospec/maap-py.git
 pushd maap-py
 pip install -e .
-echo MAAP_CONF=${PWD} >> ~/.bashrc
-popd
